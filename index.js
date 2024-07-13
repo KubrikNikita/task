5b01dbeca40988040d1a97b469d073d3ab6395e9
nikita <kubrik2@mail.ru>
2024-07-13 17:40:51 +0500

    dots

--- index.js
+++ index.js
@@ -2,44 +2,36 @@ const button = document.querySelector('#button')
 const body = document.querySelector('p')
 const title = document.querySelector('h1')
 const wrap = document.querySelector('#wrap')
-const dotsElement = document.getElementById('dots');
+const dotsElement = document.querySelector('#dots');
+let count = 0;
 
 const getPosts = async () => {
     const jsonRes = await fetch('https://jsonplaceholder.typicode.com/posts')
     const res = await jsonRes.json()
     return res;
 }
+dotsElement.classList.add("hidden")
+button.addEventListener('click', async () => {
+    dotsElement.classList.add("show");
+    count = 0;
+    const posts = await new Promise(resolve => setTimeout(async () => {
+        resolve(await getPosts());
+    }, 2000));
+    generateHtml(posts.slice(0, 19));
+    dotsElement.remove();
+})
 
-function animateDots() {
-    console.log(count);
-    count = (count + 1) % 4;
-    dots = '.'.repeat(count);
-    dotsElement.textContent = dots;
+function generateHtml(arr) {
+    for (let i = 0; i < arr.length; i++) {
+        const currentItem = arr[i]
+        const node = document.createElement("div")
+        node.innerHTML = `<h1>${currentItem.title}</h1>\n<p>${currentItem.body}</p>`
+        wrap.appendChild(node)
+    }
 }
 
-button.addEventListener('click', async (event) => {
-    event.preventDefault()
-    
-    const posts = await getPosts()
-    generateHtml(posts.slice(0, 19))
-    
 
-    dotsElement.textContent = '';
-    count = 0;
-    intervalId = setInterval(animateDots, 250);
-    function generateHtml(arr) {
-        for (let i = 0; i < arr.length; i++) {
-            const currentItem = arr[i]
-            const node = document.createElement("div")
-            setTimeout(() => {
-                node.innerHTML = `<h1>${currentItem.title}</h1>\n<p>${currentItem.body}</p>`
-                wrap.appendChild(node)
-                clearInterval(intervalIdgenerateHtml);
-                dotsElement.textContent = ''; 
-            }, 2000);
-        }
-    }
 
-})
+
 
 
