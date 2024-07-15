bf6806ab8e88f1851d08692567f90b0c4506e3c8
nikita <kubrik2@mail.ru>
2024-07-15 13:07:55 +0500

    users

--- index.js
+++ index.js
@@ -9,14 +9,17 @@ const dotsElementUsers = document.querySelector('#dots__users');
 let count = 0;
 const link = 'https://jsonplaceholder.typicode.com'
 
+
+
 const getAnything = async (value) => {
     const jsonRes = await fetch(`${link}/${value}`)
     return res = await new Promise(resolve => setTimeout(async () => {
-            resolve(await jsonRes.json());
-        }, 1000));
+        resolve(await jsonRes.json());
+    }, 1000));
 }
 
-const  getPosts  = async () => {
+
+const getPosts = async () => {
     return await getAnything('posts')
 }
 
@@ -26,45 +29,34 @@ postsButton.addEventListener('click', async () => {
     count = 0;
 
     const posts = await getPosts()
-    
-    generateHtmlPosts(posts.slice(0, 19));
+
+    generateHtml(posts.slice(0, 19), postsWrap);
     dotsElementPosts.classList.replace("show", "hidden");
 })
 
-function generateHtmlPosts(arr) {
-    for (let i = 0; i < arr.length; i++) {
-        const currentItem = arr[i]
-        const node = document.createElement("div")
-        node.innerHTML = `<h1>${currentItem.title}</h1>\n<p>${currentItem.body}</p>`
-        postsWrap.appendChild(node)
-    }
-}
+
 const getUsers = async () => {
-    const jsonRes = await fetch('https://jsonplaceholder.typicode.com/users')
-    const res = await jsonRes.json()
-    return res;
+    return await getAnything('users')
 }
 
 dotsElementUsers.classList.add("hidden")
+
 usersButton.addEventListener('click', async () => {
     dotsElementUsers.classList.add("show");
     count = 0;
-    const users = await new Promise(resolve => setTimeout(async () => {
-        resolve(await getUsers());
-    }, 2000));
-    generateHtmlUsers(users);
+    const users = await getUsers();
+    const userToTitle = users.map((item) => {
+        const newObj = {}
+        newObj.title = item.name
+        newObj.body = item.username
+        return newObj
+    })
+    generateHtml(userToTitle, usersWrap);
     dotsElementUsers.classList.replace("show", "hidden");
 })
-function generateHtmlUsers(arr) {
-    for (let i = 0; i < arr.length; i++) {
-        const currentItem = arr[i]
-        const node = document.createElement("div")
-        node.innerHTML = `<h1>${currentItem.name}</h1>\n<p>${currentItem.username}</p>`
-        usersWrap.appendChild(node)
-    }
-}
 
 
+ 
 
 
 
