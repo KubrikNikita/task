7d1365e093ca5fb6868b7de3ba8fd6e67701da9b
nikita <kubrik2@mail.ru>
2024-07-15 10:16:23 +0500

    get any

--- index.js
+++ index.js
@@ -7,19 +7,26 @@ const usersButton = document.querySelector('#users__button')
 const usersWrap = document.querySelector('#users__wrap')
 const dotsElementUsers = document.querySelector('#dots__users');
 let count = 0;
+const link = 'https://jsonplaceholder.typicode.com'
 
-const getPosts = async () => {
-    const jsonRes = await fetch('https://jsonplaceholder.typicode.com/posts')
-    const res = await jsonRes.json()
-    return res;
+const getAnything = async (value) => {
+    const jsonRes = await fetch(`${link}/${value}`)
+    return res = await new Promise(resolve => setTimeout(async () => {
+            resolve(await jsonRes.json());
+        }, 1000));
 }
+
+const  getPosts  = async () => {
+    return await getAnything('posts')
+}
+
 dotsElementPosts.classList.add("hidden")
 postsButton.addEventListener('click', async () => {
     dotsElementPosts.classList.add("show");
     count = 0;
-    const posts = await new Promise(resolve => setTimeout(async () => {
-        resolve(await getPosts());
-    }, 2000));
+
+    const posts = await getPosts()
+    
     generateHtmlPosts(posts.slice(0, 19));
     dotsElementPosts.classList.replace("show", "hidden");
 })
@@ -45,7 +52,7 @@ usersButton.addEventListener('click', async () => {
     const users = await new Promise(resolve => setTimeout(async () => {
         resolve(await getUsers());
     }, 2000));
-    generateHtmlUsers(users.slice(0, 19));
+    generateHtmlUsers(users);
     dotsElementUsers.classList.replace("show", "hidden");
 })
 function generateHtmlUsers(arr) {
