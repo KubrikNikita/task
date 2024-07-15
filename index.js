b609fbcb38914c2bbb9e15bda8bc4982f4b19566
nikita <kubrik2@mail.ru>
2024-07-15 21:17:52 +0500

    input

--- index.js
+++ index.js
@@ -2,12 +2,14 @@ const postsButton = document.querySelector('#posts__button')
 const body = document.querySelector('p')
 const title = document.querySelector('h1')
 const postsWrap = document.querySelector('#posts__wrap')
-const dotsElementPosts = document.querySelector('#dots__posts');
 const usersButton = document.querySelector('#users__button')
 const usersWrap = document.querySelector('#users__wrap')
-const dotsElementUsers = document.querySelector('#dots__users');
-let count = 0;
+const userIdWrap = document.querySelector('#user__id_wrap');
+const userIdButton = document.querySelector('#user__id_button');
+const userIdInput = document.querySelector('#user__id_input');
+const dots = document.querySelector('.dots')
 const link = 'https://jsonplaceholder.typicode.com'
+let count = 0;
 
 
 
@@ -23,26 +25,24 @@ const getPosts = async () => {
     return await getAnything('posts')
 }
 
-dotsElementPosts.classList.add("hidden")
 postsButton.addEventListener('click', async () => {
-    dotsElementPosts.classList.add("show");
+    generateLoader(postsWrap);
     count = 0;
-
     const posts = await getPosts()
-
+    if  (postsWrap !== undefined) {
+        postsWrap.innerHTML = ''
+    } 
     generateHtml(posts.slice(0, 19), postsWrap);
-    dotsElementPosts.classList.replace("show", "hidden");
+   
 })
-
+dots.remove()
 
 const getUsers = async () => {
     return await getAnything('users')
 }
 
-dotsElementUsers.classList.add("hidden")
-
 usersButton.addEventListener('click', async () => {
-    dotsElementUsers.classList.add("show");
+    generateLoader(usersWrap);
     count = 0;
     const users = await getUsers();
     const userToTitle = users.map((item) => {
@@ -51,12 +51,58 @@ usersButton.addEventListener('click', async () => {
         newObj.body = item.username
         return newObj
     })
+    if  (usersWrap !== undefined) {
+        usersWrap.innerHTML = ''
+    } 
     generateHtml(userToTitle, usersWrap);
-    dotsElementUsers.classList.replace("show", "hidden");
+})
+
+const getUserById = async () => {
+    return await getAnything(`users/${userIdInput.value}`)
+
+}
+
+userIdButton.addEventListener('click', async () => {
+    generateLoader(userIdWrap);
+    count = 0;
+    const user = await getUserById();
+    if (!user?.name) {
+        userIdWrap.innerHTML = `<h1>Юзер не найден</h1>`
+        return;
+    }
+    if  (userIdWrap !== undefined) {
+        userIdWrap.innerHTML = ''
+    } 
+    generateHtmlUserId(user, userIdWrap);
 })
 
 
- 
+function generateHtml(arr, wrap) {
+    for (let i = 0; i < arr.length; i++) {
+        const currentItem = arr[i]
+        const node = document.createElement("div")
+        node.innerHTML = `<h1>${currentItem.title}</h1>\n<p>${currentItem.body}</p>`
+        wrap.appendChild(node)
+    }
+    
+}
+
+function generateHtmlUserId(obj, wrap) {
+    const node = document.createElement("div")
+    wrap.appendChild(node)
+    node.innerHTML = `<h1>${obj.name}</h1>\n<p>${obj.username}</p>`
+}
+
+function generateLoader(wrap) {
+    const node = document.createElement("div")
+    node.classList.add("dots")
+    node.innerHTML = `<div class="dot"></div>\n<div class="dot"></div>\n<div class="dot"></div>`;
+    wrap.appendChild(node)
+    
+}
+
+
+
 
 
 
