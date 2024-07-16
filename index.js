d2753a1fddba3f9dd26215f285ecf9be819090b7
nikita <kubrik2@mail.ru>
2024-07-16 20:06:52 +0500

    input without button

--- index.js
+++ index.js
@@ -1,96 +1,54 @@
-const postsButton = document.querySelector('#posts__button')
-const body = document.querySelector('p')
-const title = document.querySelector('h1')
-const postsWrap = document.querySelector('#posts__wrap')
-const usersButton = document.querySelector('#users__button')
-const usersWrap = document.querySelector('#users__wrap')
 const userIdWrap = document.querySelector('#user__id_wrap');
-const userIdButton = document.querySelector('#user__id_button');
 const userIdInput = document.querySelector('#user__id_input');
+const but = document.querySelector("#user__id_button")
 const dots = document.querySelector('.dots')
-const link = 'https://jsonplaceholder.typicode.com'
+const link = 'https://api.edamam.com/search?app_id=900da95e&app_key=40698503668e0bb3897581f4766d77f9&'
+const nameLink = 'q='
 let count = 0;
-
-
-
 const getAnything = async (value) => {
-    const jsonRes = await fetch(`${link}/${value}`)
+    const jsonRes = await fetch(`${link}${nameLink}${value}&`)
     return res = await new Promise(resolve => setTimeout(async () => {
         resolve(await jsonRes.json());
     }, 1000));
 }
-
-
-const getPosts = async () => {
-    return await getAnything('posts')
+const getItem = async () => {
+    return await getAnything(`${userIdInput.value}`)
 }
 
-postsButton.addEventListener('click', async () => {
-    generateLoader(postsWrap);
-    count = 0;
-    const posts = await getPosts()
-    if  (postsWrap !== undefined) {
-        postsWrap.innerHTML = ''
-    } 
-    generateHtml(posts.slice(0, 19), postsWrap);
-   
-})
-dots.remove()
-
-const getUsers = async () => {
-    return await getAnything('users')
-}
-
-usersButton.addEventListener('click', async () => {
-    generateLoader(usersWrap);
-    count = 0;
-    const users = await getUsers();
-    const userToTitle = users.map((item) => {
-        const newObj = {}
-        newObj.title = item.name
-        newObj.body = item.username
-        return newObj
-    })
-    if  (usersWrap !== undefined) {
-        usersWrap.innerHTML = ''
-    } 
-    generateHtml(userToTitle, usersWrap);
-})
-
-const getUserById = async () => {
-    return await getAnything(`users/${userIdInput.value}`)
-
-}
-
-userIdButton.addEventListener('click', async () => {
-    generateLoader(userIdWrap);
-    count = 0;
-    const user = await getUserById();
-    if (!user?.name) {
-        userIdWrap.innerHTML = `<h1>Юзер не найден</h1>`
-        return;
+const debounce = (fn, ms) => {
+    let timeout;
+    return function() {
+        const fnCall = () => {fn.apply(this, arguments)}
+        clearTimeout(timeout);
+        timeout = setTimeout(fnCall, ms);
     }
-    if  (userIdWrap !== undefined) {
-        userIdWrap.innerHTML = ''
-    } 
-    generateHtmlUserId(user, userIdWrap);
-})
-
-
-function generateHtml(arr, wrap) {
-    for (let i = 0; i < arr.length; i++) {
-        const currentItem = arr[i]
-        const node = document.createElement("div")
-        node.innerHTML = `<h1>${currentItem.title}</h1>\n<p>${currentItem.body}</p>`
-        wrap.appendChild(node)
-    }
+}
+func = async() => {
+        generateLoader(userIdWrap)
+        const item = await getItem();
+        if (!item?.q){
+            userIdWrap.innerHTML = `<h1>Рецепт не найден</h1>`
+            return;
+        }
+        if  (userIdWrap !== undefined) {
+            userIdWrap.innerHTML = ''
+        }
     
-}
+        for (let i = 0; i<10; i++){
+            generateHtmlItem(item.hits[i].recipe.label,item.hits[i].recipe.healthLabels, userIdWrap)
+        }
+        return
+    }
 
-function generateHtmlUserId(obj, wrap) {
+
+func = debounce(func, 1000);
+userIdInput.addEventListener('keyup',func)
+
+function generateHtmlItem(label, healthLabels, wrap) {
+
     const node = document.createElement("div")
     wrap.appendChild(node)
-    node.innerHTML = `<h1>${obj.name}</h1>\n<p>${obj.username}</p>`
+    node.innerHTML = `<h1>${label}</h1><p>${healthLabels}</p>`
 }
 
 function generateLoader(wrap) {
@@ -100,6 +58,8 @@ function generateLoader(wrap) {
     wrap.appendChild(node)
     
 }
+
+
 
 
 
